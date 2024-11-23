065a662da734136f6b6a04d6f7f91e36f9b8fdde
ad_itya07 <jhaaditya2207@gmail.com>
2024-11-23 19:39:36 +0530

    Adde privacy policy and Terms of service route

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,9 +1,36 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<span>HOME PAGE</span>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/privacy-policy"
+        element={
+          <Layout>
+            <PrivacyPolicy />
+          </Layout>
+        }
+      />
+      <Route
+        path="/terms-of-service"
+        element={
+          <Layout>
+            <TermsOfService />
+          </Layout>
+        }
+      />
       <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
