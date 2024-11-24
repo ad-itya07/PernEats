0cce399916c82e3c2a6b989b5bd88fd9e44265fc
ad_itya07 <jhaaditya2207@gmail.com>
2024-11-24 11:53:52 +0530

    added Auth0 config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {BrowserRouter} from "react-router-dom";
-import AppRoutes from './AppRoutes.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./AppRoutes.tsx";
+import Auth0ProviderWithNavigate from "./Auth/Auth0ProviderWithNavigate.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
