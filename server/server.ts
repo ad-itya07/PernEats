be7808d72122ff52c31b8b4fd770a5c07fce734f
ad_itya07 <jhaaditya2207@gmail.com>
2024-11-27 19:33:14 +0530

    added user-route

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,18 @@
-import express , {Response , Request} from "express";
+import express, { Response, Request } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { router as userRoute } from "./src/routes/userRoute";
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/" , async (req : Request, res : Response) => {})
+app.use("/api/user", userRoute);
 
 app.listen(port, () => {
-    console.log(`App running on port: ${port}`);
-})
+  console.log(`App running on port: ${port}`);
+});
