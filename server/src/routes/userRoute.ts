5cd7bbd66a1fad7cc4243d9a69dbd479314e61dc
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-19 22:22:13 +0530

    added new User controller in route

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import UserController from "../controllers/userController";
+import userController from "../controllers/userController";
 
 export const router = express.Router()
 
-router.post("/create-user", UserController.createUser)
+router.post("/create-user", userController.createUser as any);
