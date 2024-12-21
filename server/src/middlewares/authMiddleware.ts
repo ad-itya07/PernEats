dcb9119fffde555b346a37f13cda1feb6e98c2b7
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-21 08:45:10 +0530

    jwt token verification middleware using auth0

---
+++ server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,10 @@
+import { auth } from "express-oauth2-jwt-bearer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: "RS256",
+});
