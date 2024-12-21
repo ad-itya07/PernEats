4cc723a06e866ac98a1b59108f09d6db0b3225f7
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-21 08:45:52 +0530

    added jwt-check-middleware in create-user-route

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "../controllers/userController";
+import { jwtCheck } from "../middlewares/authMiddleware";
 
 export const router = express.Router()
 
-router.post("/create-user", userController.createUser as any);
+router.post("/create-user", jwtCheck, userController.createUser as any);
