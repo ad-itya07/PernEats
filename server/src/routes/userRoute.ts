0afdb463cbd83bd322f6a0af9537efd56e7436b0
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-21 14:26:39 +0530

    added update user route

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import userController from "../controllers/userController";
-import { jwtCheck } from "../middlewares/authMiddleware";
+import { jwtCheck, jwtParseUserAndAuth0IdToRequest } from "../middlewares/authMiddleware";
+import { validateMyUserRequest } from "../middlewares/validation";
 
 export const router = express.Router()
 
 router.post("/create-user", jwtCheck, userController.createUser as any);
+router.put("/update-user", jwtCheck, jwtParseUserAndAuth0IdToRequest as any, validateMyUserRequest as any, userController.updateUser as any);
