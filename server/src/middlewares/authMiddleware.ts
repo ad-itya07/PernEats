08a9876aa04f100d90fc2793bef7c1241ca79642
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-21 14:25:39 +0530

    middleware to parse userId, auth0Id to request

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,10 +1,52 @@
 import { auth } from "express-oauth2-jwt-bearer";
 import dotenv from "dotenv";
+import { NextFunction, Response, Request } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel";
 
 dotenv.config();
+
+declare global {
+  namespace Express {
+    interface Request {
+      auth0Id: string;
+      userId: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: "RS256",
 });
+
+export const jwtParseUserAndAuth0IdToRequest = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const {authorization} = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authorization.split(" ")[1];
+  
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decoded.sub;
+
+    const user = await User.findOne({ auth0Id });
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
