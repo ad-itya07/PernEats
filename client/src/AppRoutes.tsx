26e13275f68a2a36de78f65077e2f2b511d8d686
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-22 13:09:07 +0530

    updated: protected-routing

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -5,6 +5,7 @@ import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./Auth/protectedRoute";
 
 const AppRoutes = () => {
   return (
@@ -35,14 +36,17 @@ const AppRoutes = () => {
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
 
-      <Route
-        path="/user-profile"
-        element={
-          <Layout>
-            <UserProfilePage />
-          </Layout>
-        }
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
+      
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
