7d190a1b4eda9ae150892c5a4e94230042a4b9db
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-23 21:18:53 +0530

    added: create-Restaurant controller

---
+++ server/src/controllers/restaurantController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import Restaurant from "../models/restaurant";
+import { upload } from "../middlewares/uploadMulter";
+import mongoose from "mongoose";
+import cloudinary from "cloudinary";
+
+const createRestaurant = async (req: Request, res: Response) => {
+  try {
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
+
+    if (existingRestaurant) {
+      return res
+        .status(409)
+        .json({ message: "User restaurant already exists" });
+    }
+
+    // const imageUrl = await upload(req.file as Express.Multer.File);
+    const image = req.file as Express.Multer.File;
+    const base64Image = Buffer.from(image.buffer).toString("base64");
+    const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+
+    const uploadResponse = await cloudinary.v2.uploader.upload(dataURI, {
+      folder: "Book Covers",
+      public_id: `${req.body.restaurantName}-${req.userId}`,
+    });
+
+    const restaurant = new Restaurant(req.body);
+    restaurant.imageUrl = uploadResponse.url;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    restaurant.lastUpdated = new Date();
+    await restaurant.save();
+
+    res.status(201).send(restaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export default {
+  createRestaurant,
+};
