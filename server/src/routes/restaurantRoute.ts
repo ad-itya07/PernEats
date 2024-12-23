5df2d68d26e5cd0f2c522fe563a50198fea45282
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-23 21:20:02 +0530

    added: /create route

---
+++ server/src/routes/restaurantRoute.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { upload } from "../middlewares/uploadMulter";
+import restaurantController from "../controllers/restaurantController";
+import { jwtCheck, jwtParseUserAndAuth0IdToRequest } from "../middlewares/authMiddleware";
+import { validateMyRestaurantRequest } from "../middlewares/validation";
+
+export const router = express.Router();
+
+router.post(
+  "/create",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest as any,
+  jwtCheck,
+  jwtParseUserAndAuth0IdToRequest as any,
+  restaurantController.createRestaurant as any
+);
