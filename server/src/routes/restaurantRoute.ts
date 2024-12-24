9abe30667c04b3ce6c0913a3ed9985666ea3e7ba
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-25 00:45:59 +0530

    added: get-restaurant route

--- server/src/routes/restaurantRoute.ts
+++ server/src/routes/restaurantRoute.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import { upload } from "../middlewares/uploadMulter";
 import restaurantController from "../controllers/restaurantController";
-import { jwtCheck, jwtParseUserAndAuth0IdToRequest } from "../middlewares/authMiddleware";
+import {
+  jwtCheck,
+  jwtParseUserAndAuth0IdToRequest,
+} from "../middlewares/authMiddleware";
 import { validateMyRestaurantRequest } from "../middlewares/validation";
 
 export const router = express.Router();
+
+router.get(
+  "/",
+  jwtCheck,
+  jwtParseUserAndAuth0IdToRequest as any,
+  restaurantController.getRestaurant as any
+);
 
 router.post(
   "/create",
