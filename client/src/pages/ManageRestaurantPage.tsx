931adc908415ff5611bd7aa0a3746f8dcc214c54
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-25 13:39:48 +0530

    update: onSave can handle update, create Restaurant

--- client/src/pages/ManageRestaurantPage.tsx
+++ client/src/pages/ManageRestaurantPage.tsx
@@ -1,13 +1,23 @@
-import { useCreateRestaurant, useGetRestaurant } from "@/api/RestaurantApi";
+import {
+  useCreateRestaurant,
+  useGetRestaurant,
+  useUpdateRestaurant,
+} from "@/api/RestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
-  const { createRestaurant, isLoading} = useCreateRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } = useCreateRestaurant();
   const { restaurant } = useGetRestaurant();
-  
+  const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateRestaurant();
+
+  const isEditing = !!restaurant; // If restaurant exists, we are editing. (!!) means give me a true field value.
   return (
-    <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading}/>
-  )
-}
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
+    />
+  );
+};
 
 export default ManageRestaurantPage;
