95652b371506f10a6dc16fc6524f2086fad2148c
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-25 13:38:35 +0530

    added use-Upadte-Restaurant mutation

--- client/src/api/RestaurantApi.tsx
+++ client/src/api/RestaurantApi.tsx
@@ -75,3 +75,42 @@ export const useGetRestaurant = () => {
     isLoading,
   }
 }
+
+export const useUpdateRestaurant = () => {
+  const {getAccessTokenSilently} = useAuth0();
+
+  const updateRestaurantRequest = async (restaurantFormData: FormData) : Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/restaurant/update`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    }); 
+
+    if (!response.ok) {
+      throw new Error("Failed to update restaurant");
+    }
+
+    return response.json();
+  }
+
+  const {
+    mutate: updateRestaurant,
+    isLoading,
+    isSuccess,
+    error,
+  } = useMutation(updateRestaurantRequest);
+
+  if (isSuccess) {
+    toast.success("Restaurant updated!");
+  }
+
+  if (error) {
+    toast.error("Unable to update restaurant");
+  }
+
+  return {updateRestaurant, isLoading};
+}
