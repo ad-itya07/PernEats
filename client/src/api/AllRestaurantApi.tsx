5f8d6fc141d502b243f5f9da4ca9f0e9352f2491
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-27 01:35:32 +0530

    added: get-Restaurant-query

--- client/src/api/AllRestaurantApi.tsx
+++ client/src/api/AllRestaurantApi.tsx
@@ -1,18 +1,23 @@
 import { SearchState } from "@/pages/SearchPage";
-import { RestaurantSearchResponse } from "@/types/restaurantType";
+import { Restaurant, RestaurantSearchResponse } from "@/types/restaurantType";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useSearchRestaurants = (searchState: SearchState, city?: string) => {
-  const createSearchRequest = async () : Promise<RestaurantSearchResponse>=> {
+export const useSearchRestaurants = (
+  searchState: SearchState,
+  city?: string
+) => {
+  const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
     const params = new URLSearchParams();
     params.set("searchQuery", searchState.searchQuery);
     params.set("page", searchState.page.toString());
     params.set("selectedCuisines", searchState.selectedCuisines.join(","));
     params.set("sortOption", searchState.sortOption);
 
-    const response = await fetch(`${API_BASE_URL}/api/all/restaurant/search/${city}?${params.toString()}`);
+    const response = await fetch(
+      `${API_BASE_URL}/api/all/restaurant/search/${city}?${params.toString()}`
+    );
 
     if (!response.ok) {
       throw new Error("Failed to get restaurant");
@@ -21,9 +26,7 @@ export const useSearchRestaurants = (searchState: SearchState, city?: string) =>
     return response.json();
   };
 
-  const { 
-    data: results, 
-    isLoading } = useQuery(
+  const { data: results, isLoading } = useQuery(
     ["searchRestaurants", searchState], // this searchState here tells the useQuery that everytime the searchState change, we have to do another request
     createSearchRequest,
     { enabled: !!city } // only fetch when city is provided
@@ -31,6 +34,31 @@ export const useSearchRestaurants = (searchState: SearchState, city?: string) =>
 
   return {
     results,
+    isLoading,
+  };
+};
+
+export const useGetRestaurant = (restaurantId?: string) => {
+  const getRestaurantRequest = async (): Promise<Restaurant> => {
+    const response = await fetch(
+      `${API_BASE_URL}/api/all/restaurant/${restaurantId}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant");
+    }
+
+    return response.json();
+  };
+
+  const { data: restaurant, isLoading } = useQuery(
+    "fetchRestaurant",
+    getRestaurantRequest,
+    { enabled: !!restaurantId }
+  );
+
+  return {
+    restaurant,
     isLoading,
   };
 };
