6794e669bb57f5c8fcef7a203c0fd883a96c2578
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-27 12:40:24 +0530

    order-Route

---
+++ server/src/routes/orderRoute.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  jwtCheck,
+  jwtParseUserAndAuth0IdToRequest,
+} from "../middlewares/authMiddleware";
+import orderController from "../controllers/orderController";
+
+export const router = express.Router();
+
+router.post(
+  "/checkout/create-checkout-session",
+  jwtCheck,
+  jwtParseUserAndAuth0IdToRequest as any,
+  orderController.createCheckoutSession as any
+);
