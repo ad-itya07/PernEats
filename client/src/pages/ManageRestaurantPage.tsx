8fe9f20266a22dc7a75ac528cfcd4c68df411dc0
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-28 00:14:12 +0530

    added: Tabs to sepearte Restaurant-Orders and Manage-Restaurant

--- client/src/pages/ManageRestaurantPage.tsx
+++ client/src/pages/ManageRestaurantPage.tsx
@@ -1,22 +1,43 @@
 import {
   useCreateRestaurant,
+  useGetMyRestaurantOrders,
   useGetRestaurant,
   useUpdateRestaurant,
 } from "@/api/RestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } = useCreateRestaurant();
   const { restaurant } = useGetRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateRestaurant();
+  const { orders } = useGetMyRestaurantOrders();
 
   const isEditing = !!restaurant; // If restaurant exists, we are editing. (!!) means give me a true field value.
   return (
-    <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
