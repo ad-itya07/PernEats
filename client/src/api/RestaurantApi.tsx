f5c82c0b777c4a31d560c7762b5a5e1e76da7843
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-28 00:15:26 +0530

    added: get-My-Restaurant-Orders Request

--- client/src/api/RestaurantApi.tsx
+++ client/src/api/RestaurantApi.tsx
@@ -1,9 +1,38 @@
+import { Order } from "@/types/orderCheckoutTypes";
 import { Restaurant } from "@/types/restaurantType";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyRestaurantOrders = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyRestaurantOrdersRequest = async (): Promise<Order[]> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/restaurant/order`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch orders");
+    }
+
+    return response.json();
+  };
+
+  const { data: orders, isLoading } = useQuery(
+    "fetchMyRestaurantOrders",
+    getMyRestaurantOrdersRequest
+  );
+
+  return { orders, isLoading };
+};
 
 export const useCreateRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
