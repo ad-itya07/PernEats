e14695f7e5a8765bbfee5bc299a4cc4e2da80155
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-27 13:23:42 +0530

    Checkout-Session-Request type

---
+++ client/src/types/orderCheckoutTypes.ts
@@ -0,0 +1,14 @@
+export type CheckoutSessionRequest = {
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    email: string;
+    name: string;
+    addressLine1: string;
+    city: string;
+  };
+  restaurantId: string;
+};
