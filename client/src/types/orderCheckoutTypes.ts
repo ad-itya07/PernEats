472fb0337b6cd03496e867de99b7da6573ebdfe7
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-28 00:08:54 +0530

    added: OrderStatus and Order type

--- client/src/types/orderCheckoutTypes.ts
+++ client/src/types/orderCheckoutTypes.ts
@@ -1,3 +1,6 @@
+import { Restaurant } from "./restaurantType";
+import { User } from "./userType";
+
 export type CheckoutSessionRequest = {
   cartItems: {
     menuItemId: string;
@@ -11,4 +14,31 @@ export type CheckoutSessionRequest = {
     city: string;
   };
   restaurantId: string;
+};
+
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    email: string;
+    name: string;
+    addressLine1: string;
+    city: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
 };
