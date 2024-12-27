d06da121a88e8e752bf54048d24ec9c4019e6a58
ad_itya07 <jhaaditya2207@gmail.com>
2024-12-27 09:33:04 +0530

    updated: manual-title & submit btn as props

--- client/src/forms/user-profile-form/UserProfileForm.tsx
+++ client/src/forms/user-profile-form/UserProfileForm.tsx
@@ -22,14 +22,16 @@ type Props = {
   currentUser: User;
   onSave: (userProfileData : UserFormData) => void;
   isLoading: boolean;
+  title?: string;
+  buttonText?: string;
 }
 
-const UserProfileForm = ({onSave , isLoading, currentUser}: Props) => {
+const UserProfileForm = ({onSave , isLoading, currentUser, title = "User Profile Form", buttonText = "Submit"}: Props) => {
     const form = useForm<UserFormData>({
         resolver: zodResolver(formSchema),
         defaultValues: currentUser,
     });
-
+    
     useEffect(() => {
         form.reset(currentUser);
     }, [currentUser, form]);
@@ -40,7 +42,7 @@ const UserProfileForm = ({onSave , isLoading, currentUser}: Props) => {
       className="space-y-4 bg-gray-50 rounded-lg md:p-10 border border-gray-300 shadow-md"
     >
       <div>
-        <h2 className="text-2xl font-bold">User Profile Form</h2>
+        <h2 className="text-2xl font-bold">{title}</h2>
         <FormDescription>
           View and change your profile information here
         </FormDescription>
@@ -117,7 +119,7 @@ const UserProfileForm = ({onSave , isLoading, currentUser}: Props) => {
         <LoadingButton />
       ) : (
         <Button type="submit" className="bg-orange-500">
-          Submit
+          {buttonText}
         </Button>
       )}
     </form>
